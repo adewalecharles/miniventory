70e1b11ada9b8c22735802f6ee86eb9543eb571d
adewalecharles <shyprince1@gmail.com>
2020-04-08 18:51:26 +0100

    error pages done

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Page not Found</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="E-ventory is an inventory application, keep track of all your goods, checkout with invoice" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Bootstrap Core CSS -->
+<link href="{{asset('css/bootstrap.min.css')}}" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="{{asset('css/style.css')}}" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="{{asset('css/morris.css')}}" type="text/css"/>
+<!-- Graph CSS -->
+<link href="{{asset('css/font-awesome.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}"> 
+
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="{{asset('css/icon-font.min.css')}}" type='text/css' />
+<!-- //lined-icons -->
+</head> 
+<body>
+	<div class="main">
+	<div class="container">
+		<div class="agile">
+			
+			<div class="w3l-txt">
+				<div class="text">
+					<h1>PAGE NOT FOUND</h1>	
+					
+					<p>You have been tricked into click on a link that can not be found. Please check the url or go to <a href="{{route('home')}}">main page</a> and see if you can locate what you are looking for</p>
+				</div>
+				<div class="image">
+					<img src="images/smile.png">
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="back">
+						<a href="index.html">Back to home</a>
+				</div>
+				<div class="footer">
+					<p>© 2020 MiniVentory. All Rights Reserved | Design and Developed By   <a href="https://mbh.ng" target="_blank">MybudgetHosting</a> </p>
+				</div>
+				
+					
+	</div>
+	</div>
+    </div>
+    
+    <script src="js/jquery-2.1.4.min.js"></script>
+</body>
+</html>
